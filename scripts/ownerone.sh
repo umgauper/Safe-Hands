17966077400953a9010b7baeb13793dadc7b29fc
Una Gauper <umgauper@gmail.com>
2016-04-16 09:54:55 -0700

    initial commit

---
+++ scripts/ownerone.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+npm owner add boutell $1
+npm owner add colpanik $1
+npm owner add jsumnersmith $1
+npm owner add alexgilbert $1
+npm owner add stuartromanek $1
+npm owner add mcoppola $1
+npm owner add suhmantha1 $1
+npm owner add jimmyh $1
+npm owner add kyjoya $1
+npm owner add austinstarin $1
